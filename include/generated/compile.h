679a14d95afe1598e80f05c5c871830e14a63b82
Sagformas <sagformas@gmail.com>
2015-01-24 20:06:55 +0100

    Primera subida

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 10 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#10 SMP PREEMPT Sat Jan 24 20:00:55 CET 2015"
+#define LINUX_COMPILE_BY "sagformas"
+#define LINUX_COMPILE_HOST "EPDCenter-Workstation"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
